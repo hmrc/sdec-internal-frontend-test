1b300a8543ae273193e94390cb7cb0804671106f
Christy <223317067+chrso25@users.noreply.github.com>
2026-06-25 10:19:03 +0100

    SDEC-47 - Added shell script

---
+++ run-tests.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -e
+
+BROWSER=$1
+ENVIRONMENT=$2
+
+sbt clean -Dbrowser="${BROWSER:=chrome}" -Dbrowser.usePreviousVersion=true -Denvironment="${ENVIRONMENT:=local}" "test uk.gov.hmrc.ui.specs.*" testReport

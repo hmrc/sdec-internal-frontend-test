93cb58f7b29eec77bcf55f591f1516a32776d6a0
ramindur.singh <282824933+ramindursingh@users.noreply.github.com>
2026-06-29 15:31:50 +0100

    SDEC-45 Removed unused imports

--- src/test/scala/uk/gov/hmrc/ui/driver/BrowserDriver.scala
+++ src/test/scala/uk/gov/hmrc/ui/driver/BrowserDriver.scala
@@ -2,11 +2,7 @@ package uk.gov.hmrc.ui.driver
 
 import com.typesafe.scalalogging.LazyLogging
 import org.openqa.selenium.WebDriver
-import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach, GivenWhenThen}
-import org.scalatest.featurespec.AnyFeatureSpec
-import org.scalatest.verbs.ShouldVerb
-import uk.gov.hmrc.selenium.webdriver.{Browser, Driver, ScreenshotOnFailure}
-import uk.gov.hmrc.ui.specs.BaseSpec
+import uk.gov.hmrc.selenium.webdriver.Driver
 
 trait BrowserDriver extends LazyLogging {
   def driver: WebDriver = {

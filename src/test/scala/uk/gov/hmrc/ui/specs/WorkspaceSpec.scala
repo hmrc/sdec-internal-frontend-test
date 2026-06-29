d4d959d1cab080db26dcbf63ea9bff9cdd4866d4
Christy <223317067+chrso25@users.noreply.github.com>
2026-06-29 13:55:56 +0100

    SDEC-45 - Updated WorkspaceSpec

--- src/test/scala/uk/gov/hmrc/ui/specs/WorkspaceSpec.scala
+++ src/test/scala/uk/gov/hmrc/ui/specs/WorkspaceSpec.scala
@@ -2,16 +2,24 @@ package uk.gov.hmrc.ui.specs
 
 import org.openqa.selenium.By
 import uk.gov.hmrc.ui.pages.AuthLoginPage.{driver, login}
+import uk.gov.hmrc.ui.pages.WorkspaceLandingPage
 import uk.gov.hmrc.ui.specs.tags.AcceptanceTests
 class WorkspaceSpec extends BaseSpec {
   Feature("Internal User Journey") {
+
     Scenario("Get Landing Page", AcceptanceTests) {
+
       Given("User Logins with Credential ID") // This might be the wrong way for internal HMRC staff to login for now
       login()
-      Then("The Workspace page displays")
-      println(driver.getCurrentUrl)
-      val heading = driver.findElement(By.tagName("h1"))
-      heading.getText shouldBe "SDEC Internal Dashboard"
+      When("the dashboard page loads")
+      Then("the system must display a dashboard page layout")
+      WorkspaceLandingPage.isPageDisplayed shouldBe true
+
+      And("the system must display a navigation area")
+      WorkspaceLandingPage.isWorkspaceTabDisplayed     shouldBe true
+      WorkspaceLandingPage.isNotificationsTabDisplayed shouldBe true
+      // val heading = driver.findElement(By.tagName("h1"))
+      // heading.getText shouldBe "SDEC Internal Dashboard"
 
     }
   }
